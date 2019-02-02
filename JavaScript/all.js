dcc77426c1e98c4c26364a92937776e3c824ab1d
pvt5r486 <pvt5r486@gmail.com>
2019-02-02 12:48:27 +0800

    'fix'

--- JavaScript/all.js
+++ JavaScript/all.js
@@ -10,10 +10,10 @@ function clock() {
 
 function getAngle() {
   const date = new Date();
-  // 每格秒針度數 = 當前秒數 / 60秒 * 360度
-  const SecAngle = Math.floor(date.getSeconds() / 60 * 360);
-  const MinAngle = Math.floor(date.getMinutes() / 60 * 360);
-  const HourAngle = Math.floor(date.getHours() / 12 * 360);
+  // 每格秒針度數 = 360 度 / 60秒 * 當前秒數
+  const SecAngle = 360 / 60 * date.getSeconds();
+  const MinAngle = 360 / 60 * date.getMinutes();
+  const HourAngle = 360 / 12 * date.getHours();
   return [SecAngle, MinAngle , HourAngle];
 }
 
