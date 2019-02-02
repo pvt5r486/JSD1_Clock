0be24359f0b3575d3409421590b2124913638fc4
pvt5r486 <pvt5r486@gmail.com>
2019-02-02 12:02:23 +0800

    '修改function'

--- JavaScript/all.js
+++ JavaScript/all.js
@@ -2,19 +2,19 @@ function clock() {
   const hourPoint = document.querySelector('.hour-hand');
   const minPoint = document.querySelector('.minute-hand');
   const secPoint = document.querySelector('.second-hand');
-  secPoint.style.transform = `translateX(-50%) rotate(${180 + getAngle()[0]}deg)`;
-  minPoint.style.transform = `translateX(-50%) rotate(${getAngle()[1]}deg)`;
-  hourPoint.style.transform = `translateY(-50%) rotate(${-90 + getAngle()[2]}deg)`;
+  let angleArray = getAngle();
+  secPoint.style.transform = `translateX(-50%) rotate(${180 + angleArray[0]}deg)`;
+  minPoint.style.transform = `translateX(-50%) rotate(${angleArray[1]}deg)`;
+  hourPoint.style.transform = `translateY(-50%) rotate(${-90 + angleArray[2]}deg)`;
 }
 
 function getAngle() {
   const date = new Date();
-  // 每格秒針度數 = 當前秒數 / 60個刻度 * 360度
+  // 每格秒針度數 = 當前秒數 / 60秒 * 360度
   const SecAngle = Math.floor(date.getSeconds() / 60 * 360);
   const MinAngle = Math.floor(date.getMinutes() / 60 * 360);
   const HourAngle = Math.floor(date.getHours() / 12 * 360);
-  let angleArray = [SecAngle, MinAngle , HourAngle];
-  return angleArray;
+  return [SecAngle, MinAngle , HourAngle];
 }
 
 setInterval(clock,1000);
